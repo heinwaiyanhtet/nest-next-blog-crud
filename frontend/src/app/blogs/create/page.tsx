2ba8d3ef4de4b14e97e87217b40dcd9dff982b2b
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-20 00:09:41 +0630

    use server is removed

--- frontend/src/app/blogs/create/page.tsx
+++ frontend/src/app/blogs/create/page.tsx
@@ -1,9 +1,4 @@
-"use client"
-
 import { CreateForm } from "@/app/create-form";
-
-
-
 
 export default function Blogs() {
 
@@ -12,7 +7,7 @@ export default function Blogs() {
         <div className='flex justify-center mt-5'>
 
             <div className="w-11/12	p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-            
+
                 <CreateForm />
 
             </div>
