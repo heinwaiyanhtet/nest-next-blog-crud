dbcf8d281452e144ec4f1553ef17571f4f3085b1
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-19 23:59:20 +0630

    create blog action

--- frontend/src/app/actions.tsx
+++ frontend/src/app/actions.tsx
@@ -2,19 +2,48 @@
 
 import { revalidatePath } from "next/cache";
 
-export async function createTodo(
+export async function createBlog(
     prevState: {
-      message: string;
+      message: string | undefined | null
     },
     formData: FormData,
   ) {
     
   
-    try {
+   try {
        
-      revalidatePath("/blogs");
-    } catch (e) {
+
+    const title = formData.get("title");
+    const createdBy = formData.get("createdBy");
+    const description = formData.get("description");
+
+
+    const response = await fetch(`http://localhost:3001/blogs`,
+    {
+        method:"POST",
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          createdBy,
+          description
+        }),
+    })
+
+    if(response.ok)
+    {
+        revalidatePath("/blogs");
+        return { message: `Blog created successfully` };
+    }
+    else
+    {
+      return { message: `failed to create blog` };
+    }
+    } 
+    catch (e) 
+    {
       return { message: "Failed to create todo" };
     }
-  }
+}
   
